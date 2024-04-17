1920957025db06ca75750b126d794fe47dc2878a
Graham Duthie <grahamd483@gmail.com>
2024-04-17 14:20:26 +0200

    Add callbacks tracking and modify order type in PlaceSimpleOrderApp
    In the PlaceSimpleOrderApp class, a callback counter has been introduced along with modification in the order attributes. The callback counter is incremented with each successive call, allowing for easier tracking of the lifecycle of an order. The order action has also been altered from 'buy' to 'sell', and the order type has been changed from market to limit orders, necessitating a limit price.

--- Testbed/place_order.py
+++ Testbed/place_order.py
@@ -3,30 +3,76 @@ from ibapi.client import *
 from ibapi.wrapper import *
 
 
+def increment(self):
+    self.callBackCounter += 1  # used to track callbacks ten in all
+
+
+
 class PlaceSimpleOrderApp(EClient, EWrapper):
     def __init__(self):
         EClient.__init__(self, self)
+        self.callBackCounter = 1
 
     # this callback nextValidId is called when api calls function reqIds  in EClient class, or when  initial 4
     # interaction is complete
     def nextValidId(self, orderId: int):
-        print("NExtValidID")
+        print(f"nextValidId call = {self.callBackCounter}")
         my_contract = i_details.stockContractApple(Contract())
+        increment(self)
         self.reqContractDetails(orderId, my_contract)
 
     def contractDetails(self, reqId: int, contractDetails: ContractDetails):
-        print(f"Contract details: {contractDetails.contract}")
-        print(contractDetails.contract)
+        print(f"""contractDetails call = {self.callBackCounter}
+        Contract details: {contractDetails.contract}""")
+        increment(self)
 
         my_order = Order()
         my_order.orderId = reqId
-        my_order.action = "BUY"
+        my_order.action = "SELL"
         my_order.tif = "GTC"  # Day order is default
-        my_order.orderType = "MKT"
+        my_order.orderType = "LMT"
+        my_order.lmtPrice = 160.99
         my_order.totalQuantity = 10
 
         self.placeOrder(reqId, contractDetails.contract, my_order)
-        self.disconnect()
+
+    def openOrder(self, orderId: OrderId, contract: Contract, order: Order,
+                  orderState: OrderState):
+        print(f"""
+        *********{self.callBackCounter}_ORDER_OPENED**********
+        openOrder. orderId: {orderId},
+        contract: {contract},
+         order: {order}""")
+        increment(self)
+
+    def orderStatus(self, orderId: OrderId, status: str, filled: float, remaining: float, avgFillPrice: float, permId:
+    int, parentId: int, lastFillPrice: float, clientId: int, whyHeld: str, mktCapPrice: float):
+        print(f"""
+        
+        ******{self.callBackCounter}_ORDER_STATUS******
+        orderStatus. orderId: {orderId},
+        status: {status},
+        filled: {filled},
+        remaining: {remaining},
+        avgFillPrice: {avgFillPrice},
+        lastFillPrice: {lastFillPrice}
+        permId: {permId},
+        parentId: {parentId},
+        clientId: {clientId},
+        whyHeld: {whyHeld},
+        mktCapPrice: {mktCapPrice}""")
+        increment(self)
+
+    # will callback for every separate necesssary execution:
+    def execDetails(self, reqId: int, contract: Contract, execution: Execution):
+        print(f"""
+        
+        ******{self.callBackCounter}_EXEC_DETAILS******
+        execDetails. reqId: {reqId},
+        contract: {contract},
+        execution: {execution}
+""")
+        # self.disconnect()
 
 
 app = PlaceSimpleOrderApp()
